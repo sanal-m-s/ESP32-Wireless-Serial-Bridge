411eced71221fe432d279669112bf5f6f2005e31
sanal m s <131573751+sanal-m-s@users.noreply.github.com>
2026-06-03 15:53:29 +0530

    Update esp32_server.ino

--- esp32_server/esp32_server.ino
+++ esp32_server/esp32_server.ino
@@ -41,3 +41,69 @@ void loop()
     }
   }
 }
+
+
+
+
+
+#include <WiFi.h>
+
+const char* ssid = "SANAL";
+const char* password = "1111111111";
+
+IPAddress serverIP(192.168.137.179);
+
+WiFiClient client;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("\nESP32 CLIENT STARTING");
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi Connected");
+
+  connectServer();
+}
+
+void connectServer()
+{
+  while (!client.connect(serverIP, 5000))
+  {
+    Serial.println("Connecting to server...");
+    delay(1000);
+  }
+
+  Serial.println("Connected to server");
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    Serial.println("Disconnected");
+    connectServer();
+  }
+
+  while (Serial.available())
+  {
+    char c = Serial.read();
+    client.write(c);
+  }
+
+  while (client.available())
+  {
+    char c = client.read();
+    Serial.write(c);
+  }
+}
