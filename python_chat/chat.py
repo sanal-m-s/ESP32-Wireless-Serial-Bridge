3f83f26b9a4b46dc1a8fa97401160ac919f5e9a8
sanal m s <131573751+sanal-m-s@users.noreply.github.com>
2026-06-03 15:54:21 +0530

    Update chat.py

--- python_chat/chat.py
+++ python_chat/chat.py
@@ -20,3 +20,30 @@ threading.Thread(target=receive, daemon=True).start()
 while True:
     msg = input("You: ")
     ser.write((msg + "\n").encode())
+
+
+
+
+
+import serial
+import threading
+
+PORT = "COM7"      # CHANGE THIS
+BAUD = 115200
+
+ser = serial.Serial(PORT, BAUD, timeout=1)
+
+def receive():
+    while True:
+        try:
+            data = ser.readline().decode(errors="ignore").strip()
+            if data:
+                print(f"\nRemote: {data}")
+        except:
+            pass
+
+threading.Thread(target=receive, daemon=True).start()
+
+while True:
+    msg = input("You: ")
+    ser.write((msg + "\n").encode())
